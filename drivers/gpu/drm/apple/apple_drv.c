c17c6ed0f56623462f14f763277ae553c2426514
Steve Derico <steve@bixbyapps.com>
2026-09-03 08:07:37 -0700

    drm/apple: include drm_print.h for drm_dbg_kms
    Fixes compile error from oob_hotplug debug conversion.

--- drivers/gpu/drm/apple/apple_drv.c
+++ drivers/gpu/drm/apple/apple_drv.c
@@ -27,6 +27,7 @@
 #include <drm/drm_fb_helper.h>
 #include <drm/drm_fbdev_dma.h>
 #include <drm/drm_fb_dma_helper.h>
+#include <drm/drm_print.h>
 #include <drm/drm_gem_dma_helper.h>
 #include <drm/drm_gem_framebuffer_helper.h>
 #include <drm/drm_simple_kms_helper.h>
